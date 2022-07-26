261d7010874b4330e71f0c036accbe6adb1f8207
danieldannitz <danielgnlmenezes@gmail.com>
2022-07-26 10:44:02 -0300

    add todo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
+import { Todos } from "./components/Todos";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
+      <Todos />
     </div>
   );
 }
