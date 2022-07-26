26db78dd4bbee468d1bfc088410f352ff7e70717
danieldannitz <danielgnlmenezes@gmail.com>
2022-07-26 09:51:53 -0300

    inclduing component header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header";
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <Header />
     </div>
   );
 }
